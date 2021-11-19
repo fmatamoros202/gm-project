91d3a42563460991bcfe996d07160e9147ff4dd8
Clara Zhang <claraxixi@gmail.com>
2021-11-19 07:43:54 -0500

    header first draft

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import PageNav from '../../component/PageNav/PageNav'
 
 function Home() {
     return (
         <div>
+            <PageNav />
             <p>Welcome</p>
         </div>
     )
