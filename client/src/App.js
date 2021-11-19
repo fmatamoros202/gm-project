588f297cc467da186bfb8ee92f2137ce216aad30
Fernando Matamoros <fmatamoros202@gmail.com>
2021-11-18 22:07:43 -0500

    created a simple landing page with a log-in. No styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import {Component} from "react";
 import {Switch, Route} from 'react-router-dom';
+import LandingPage from './pages/LandingPage/LandingPage'
 
 function App() {
   return (
     <>
       <Switch>
-        <Route path='/' exact/>
+        <Route path='/' exact component={LandingPage}/>
         <Route path='/users/userId'/>
       </Switch>
     </>
---
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,17 @@
+let LandingPage = (props)=>{
+
+    return(
+        <>
+            <h1>GM's Emission Tracker App</h1>
+            <h2>Log In</h2>
+            <form>
+                <label>User Name</label>
+                <input type='text'></input>
+                <label>Password</label>
+                <input type="text"></input>
+            </form>
+        </>
+    )
+};
+
+export default LandingPage;
