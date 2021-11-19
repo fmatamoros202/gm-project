d8c1859f996733eddcad4efdb235eb5a52fe657d
Clara Zhang <claraxixi@gmail.com>
2021-11-19 09:23:19 -0500

    date components done

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import CarDetails from '../../components/CarDetails/CarDetails'
 import React from 'react';
-import PageNav from '../../component/PageNav/PageNav'
-import DayWeekMonthButtons from '../../component/DayWeekMonthButtons/DayWeekMonthButtons'
+import PageNav from '../../components/PageNav/PageNav'
+import DayWeekMonthButtons from '../../components/DayWeekMonthButtons/DayWeekMonthButtons'
 function Home() {
     return (
         <div>
             <PageNav />
-            <DayWeekMonthButtons />
-            <p>Welcome</p>
             <CarDetails></CarDetails>
+            <DayWeekMonthButtons />
         </div>
     )
 }
