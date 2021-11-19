a65f08d84b7cad0002ed1e2e0f9b9107513e28ae
Clara Zhang <claraxixi@gmail.com>
2021-11-19 08:12:34 -0500

    folder structure done

---
+++ client/src/component/DayWeekMonthButtons/DayWeekMonthButtons.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DayWeekMonthButtons.scss';
+
+function DayWeekMonthButtons() {
+    return (
+        <div>
+            hi
+        </div>
+    )
+}
+
+export default DayWeekMonthButtons
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PageNav from '../../component/PageNav/PageNav'
-
+import DayWeekMonthButtons from '../../component/DayWeekMonthButtons/DayWeekMonthButtons'
 function Home() {
     return (
         <div>
             <PageNav />
+            <DayWeekMonthButtons />
             <p>Welcome</p>
         </div>
     )
