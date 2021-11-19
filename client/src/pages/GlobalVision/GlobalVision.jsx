c6d48779b8175f07a480a20624aef36f76fce2ea
Mahalakshmi Jegadeesan <mahainfinity@gmail.com>
2021-11-19 12:43:17 -0500

    Added Banner in Vision page

--- client/src/pages/GlobalVision/GlobalVision.jsx
+++ client/src/pages/GlobalVision/GlobalVision.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './GlobalVision.scss';
+import VisionBanner from '../../assets/images/VisionBanner.png'
+import PageNav from "../../components/PageNav/PageNav";
+import BottomNav from "../../components/BottomNav/BottomNav";
 
 function GlobalVision() {
     return (
-        <div>
-            <p>vision</p>
+        <div className="vision">
+            <PageNav />
+			<img className="vision__banner" src={VisionBanner} alt="User Car"></img>
+            <BottomNav></BottomNav>
         </div>
     );
 }
