9020232a31921f3e0001da72eda28f9fcd465668
Mahalakshmi Jegadeesan <mahainfinity@gmail.com>
2021-11-19 06:39:36 -0500

    Added login functionality and assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import {Component} from "react";
 import {Switch, Route} from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <>
       <Switch>
         <Route path='/' exact component={LandingPage}/>
-        <Route path='/users/userId'/>
+        <Route path='/:UserName' component={Home}/>
       </Switch>
     </>
   );
--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,26 +1,41 @@
 import './LandingPage.scss';
 import gmlogo from '../../assets/images/gm-logo.svg';
+import { Component } from 'react';
 
 
-let LandingPage = (props)=>{
 
-    return(
-        <>
-            <div className ="main-container">
-                <div className ="container-form">
-                    <h2 className="header"><img className="header__logo" src={gmlogo} alt="GM's logo"/>GM's Emissions Tracker</h2>
-                    <h3 className="subheader">Log-In</h3>
-                    <form className="form">
-                        <label className="form__label">User Name</label>
-                        <input className="form__input" type='text'></input>
-                        <label className="form__label">Password</label>
-                        <input className="form__input" type="text"></input>
-                        <div className="form__cta">Sign-In</div>
-                    </form>
+class LandingPage extends Component{
+
+    loginHandle = (e) =>{
+        e.preventDefault();
+        if (e.target.username.value==="gmuser" && e.target.password.value==='gmuser'){
+            let path = `/${e.target.username.value}`;
+            this.props.history.push(path);
+        }
+        else{
+            alert('Please check your username and password')
+        }
+    }
+
+    render(){
+        return(
+            <>
+                <div className ="main-container">
+                    <div className ="container-form">
+                        <h2 className="header"><img className="header__logo" src={gmlogo} alt="GM's logo"/>GM's Emissions Tracker</h2>
+                        <h3 className="subheader">Log-In</h3>
+                        <form className="form" onSubmit={this.loginHandle}>
+                            <label className="form__label">User Name</label>
+                            <input name='username' className="form__input" type='text'></input>
+                            <label className="form__label">Password</label>
+                            <input type='password' name='password' className="form__input" type="text"></input>
+                            <input type="submit" className="form__cta" value="Sign-In"></input>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 };
 
 export default LandingPage;
