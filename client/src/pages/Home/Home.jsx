c02502abb2733d1d9f81828c48d5ff0da26be2f0
Jason <jtaylor14@athabasca.edu>
2021-11-19 11:15:51 -0400

    Added styling

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,15 +1,17 @@
-import CarDetails from '../../components/CarDetails/CarDetails'
-import React from 'react';
-import PageNav from '../../component/PageNav/PageNav'
+import CarDetails from "../../components/CarDetails/CarDetails";
+import React from "react";
+import PageNav from "../../components/PageNav/PageNav";
+import BottomNav from "../../components/BottomNav/BottomNav";
 
 function Home() {
-    return (
-        <div>
-            <PageNav />
-            <p>Welcome</p>
-            <CarDetails></CarDetails>
-        </div>
-    )
+	return (
+		<div>
+			<PageNav />
+			<p>Welcome</p>
+			<CarDetails></CarDetails>
+			<BottomNav></BottomNav>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
