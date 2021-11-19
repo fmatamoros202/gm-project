389a7e29f23e7c5f536bd97d01e1e71df651d0e3
Mahalakshmi Jegadeesan <mahainfinity@gmail.com>
2021-11-19 14:47:25 -0500

    added nav for logo

--- client/src/components/PageNav/PageNav.jsx
+++ client/src/components/PageNav/PageNav.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import logo from '../../../src/assets/images/logo.png';
 import hamburger from '../../../src/assets/images/Hamburger_icon.png';
 import './PageNav.scss';
+import {Link} from 'react-router-dom'
 
 
 function PageNav() {
     return (
         <div className="pagenav">
             <div className="pagenav__container">
+                <Link to="/gmuser">
                 <img className="pagenav__logo" src={logo} alt="page logo" />
+                </Link>
                 <p className="pagenav__title">ECO TRACK</p>
                 <img className="pagenav__hamburger" src={hamburger} alt="hamburger" />
             </div>
